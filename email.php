2930edaaa946ca436ee17ca9894ef19587e9bdb9
Ryzen Amd <r5rx5600ma@gmail.com>
2025-12-15 05:06:16 +0530

    added email.php sending mail UI structure

---
+++ email.php
@@ -0,0 +1,35 @@
+<table style="width: 100%; font-family: sans-serif;">
+    <tbody></tbody>
+    <tr>
+        <td align="center">
+
+            <table style="max-width: 600px;">
+                <tr>
+                    <td align="center">
+                        <a href="#" style="font-weight: bold; color:black; font-size: 35px; text-decoration: none;">ONLINE STORE</a>
+                        <hr>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <h1 style="font-size: 25px; font-weight: bold; line-height: 45px;">Reset Your Password</h1>
+                        <p style="margin-bottom: 24px;">To reset your password, please click the link below:</p>
+                        <div>
+                            <a href="#" style="display: inline-block; background-color: blue; color: white; border-radius: 8px; padding: 10px 20px; text-decoration: none;"><span>Reset Password</span></a>
+                        </div>
+                        <p style="margin-top: 24px;">If you did't request this, please ignore this email.</p>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td align="center">
+                        <hr>
+                        <p style="font-size: 14px; color: gray;">&copy; 2024 Online Store. All rights reserved.</p>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+    </tbody>
+</table>
