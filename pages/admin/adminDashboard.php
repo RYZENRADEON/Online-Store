e02309448a0ffeb52f27a7d2e754a0b94cee4b9f
RYZEN AMD <151421332+RYZENRADEON@users.noreply.github.com>
2025-12-17 00:42:14 +0530

    Modify adminDashboard.php and adminHeader.php

--- pages/admin/adminDashboard.php
+++ pages/admin/adminDashboard.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard | Online Store</title>
 
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="icon" href="../../assets/images/logo/logo01.png">
+    <link rel="stylesheet" href="../../assets/css/style.css">
+    <link rel="stylesheet" href="../../assets/css/bootstrap.css">
 </head>
 <body>
-    
-    <script src="assets/js/script.js"></script>
+    <!-- Include the admin header -->
+    <?php include 'adminHeader.php'; ?>
+
+    <script src="../../assets/js/script.js"></script>
 </body>
 </html>
--- pages/admin/adminHeader.php
+++ pages/admin/adminHeader.php
@@ -1,17 +1,25 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<!-- navbar option1 -->
+<nav class="navbar navbar-expand-lg bg-body-tertiary ">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="/Online-Store/pages/admin/adminDashboard.php">
+      <img src="/Online-Store/assets/images/logo/logo01.png" alt="" class="me-3" height="50"/>
+    </a>
+
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
+
         <li class="nav-item">
           <a class="nav-link" href="#">Link</a>
         </li>
+
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
@@ -25,30 +33,54 @@
             <li><a class="dropdown-item" href="#">Something else here</a></li>
           </ul>
         </li>
+
         <li class="nav-item">
           <a class="nav-link disabled" aria-disabled="true">Disabled</a>
         </li>
+
       </ul>
+
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
+
     </div>
+
   </div>
 </nav>
-<!--  -->
-<nav class="nav navbar-extends-lg navbar-dark fixed-top">
+<!-- navbar option2 -->
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top d-none">
   <div class="container">
-    <a href="/pages/admin/adminDashboard.php" class="navbar-brand h1 mb-0"><img src="/assets/images/logo/logo01.png" alt="" class="me-3" height="50"></a>
-    <button class="navbar-toggler" type="button" data-bs-target="#navbarSupportContent" data-bs-toggle="collapse" aria-controls="navbarSupportContent" aria-expanded="false" aria-label="Toggle navigation">
+    <a href="/Online-Sttore/pages/admin/adminDashboard.php" class="navbar-brand h1 mb-0">
+      <img src="/Online-Store/assets/images/logo/logo01.png" alt="" class="me-3" height="50"/>
+    </a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportContent" aria-controls="navbarSupportContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapsed" id="navbarSupportContent">
+
+    <div class="collapse navbar-collapse" id="navbarSupportContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-5">
+
         <li class="nav-item me-5">
           <a href="" class="nav-link active" aria-current="page">User managment</a>
         </li>
+
+        <li class="nav-item me-5">
+          <a href="" class="nav-link active" aria-current="page">Product managment</a>
+        </li>
+
+        <li class="nav-item me-5">
+          <a href="" class="nav-link active" aria-current="page">Stock managment</a>
+        </li>
+
+        <li class="nav-item me-5">
+          <a href="" class="nav-link active" aria-current="page">Reports</a>
+        </li>
+
       </ul>
     </div>
+
   </div>
 </nav>
