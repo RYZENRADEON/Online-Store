7b5df3606954d04fbe5a4373b1e6074e2df01a1e
RYZEN AMD <151421332+RYZENRADEON@users.noreply.github.com>
2025-12-16 13:35:25 +0530

    adminSignin.php added

---
+++ adminSignIn.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Sigh In | Online Store</title>
+
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="bootstrap.css">
+</head>
+
+<body>
+    <!-- ADMIN SIGN IN BOX -->
+    <div class="container">
+        <div class="row vh-100 d-flex justify-content-center align-items-center">
+            <div class="col-10 col-lg-4" id="signinBox">
+                <div class="row card">
+                    <div class="card-body">
+
+                        <div class="col-12 text-center">
+                            <h2>Admin Sign In</h2>
+                        </div>
+
+                        <div class="col-12 mb-3">
+                            <label for="adSiEmail" class="form-label">Email Address</label>
+                            <input type="email" class="form-control" id="adSiEmail">
+                        </div>
+
+                        <div class="col-12 mb-3">
+                            <label for="adSiPassword" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="adSiPassword">
+                        </div>
+
+                        <div class="errorMsgDiv3 d-none">
+                            <div class="alert alert-danger" id="errorMsg3">
+                                <!-- Error Message -->
+                            </div>
+                        </div>
+
+                        <div class="d-grid mb-3">
+                            <button class="btn btn-secondary" id="signinBtn">Sign In</button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ADMIN SIGN IN BOX -->
+
+    <script src="script.js"></script>
+</body>
+
+</html>
