577d4fbea8126099e6adbe2b82b72151279e974a
RYZEN AMD <151421332+RYZENRADEON@users.noreply.github.com>
2025-12-18 04:04:18 +0530

    modified

--- pages/admin/adminFooter.php
+++ pages/admin/adminFooter.php
@@ -1,4 +1,4 @@
-<div class="fixed-bottom col-12 text-center py-2">
+<footer class="fixed-bottom col-12 text-center py-2">
     <hr>
     <p style="font-size: 14px; color: gray;">&copy; 2024 Online Store. All rights reserved.</p>
-</div>
+</footer>
