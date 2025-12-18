753fbb85e50940ade1020ac9e7e6966ba235ca23
RYZEN AMD <151421332+RYZENRADEON@users.noreply.github.com>
2025-12-18 23:17:18 +0530

    db query modified

--- pages/admin/registerColorProcess.php
+++ pages/admin/registerColorProcess.php
@@ -6,12 +6,12 @@ $colorName = $_POST["colorName"];
 if(empty($colorName)) {
     echo "Color name cannot be empty";
 } else {
-    $rs = Database::search("SELECT * FROM `color` WHERE `name` = '".$colorName."'");
+    $rs = Database::search("SELECT * FROM `color` WHERE `color_name` = '".$colorName."'");
     $num = $rs -> num_rows;
     if($num > 0) {
         echo "Color already exists";
     } else {
-        Database::iud("INSERT INTO `color` (`name`) VALUES ('".$colorName."')");
+        Database::iud("INSERT INTO `color` (`color_name`) VALUES ('".$colorName."')");
         echo "success";
     }
 }
