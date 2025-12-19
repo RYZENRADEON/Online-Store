0427cfdafa3065d1d4831ca618b7aefb805f8784
RYZEN AMD <151421332+RYZENRADEON@users.noreply.github.com>
2025-12-19 13:32:19 +0530

    added register product modal and modified

--- pages/admin/adminProduct.php
+++ pages/admin/adminProduct.php
@@ -1,4 +1,6 @@
 <?php
+include  "../../config/connection.php";
+
 session_start();
 if (isset($_SESSION["admin"])) {
 ?>
@@ -27,7 +29,7 @@ if (isset($_SESSION["admin"])) {
                     <div class="row">
 
                         <div class="col-12 d-flex justify-content-center mb-2">
-                            <button class="btn btn-sm btn-primary mb-3">Register Product</button>
+                            <button class="btn btn-sm btn-primary mb-3" data-bs-toggle="modal" data-bs-target="#registerProductModal">Register Product</button>
                         </div>
 
                         <div class="col-6 offset-3 d-flex justify-content-center mb-3">
@@ -72,6 +74,7 @@ if (isset($_SESSION["admin"])) {
                     </div>
                 </div>
             </div>
+            <!-- Register Brand Modal -->
 
             <!-- Register Color Modal -->
             <div class="modal fade" id="registerColorModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -94,6 +97,7 @@ if (isset($_SESSION["admin"])) {
                     </div>
                 </div>
             </div>
+            <!-- Register Color Modal -->
 
             <!-- Register Category Modal -->
             <div class="modal fade" id="registerCategoryModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -116,6 +120,7 @@ if (isset($_SESSION["admin"])) {
                     </div>
                 </div>
             </div>
+            <!-- Register Category Modal -->
 
             <!-- Register Size Modal -->
             <div class="modal fade" id="registerSizeModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -138,6 +143,114 @@ if (isset($_SESSION["admin"])) {
                     </div>
                 </div>
             </div>
+            <!-- Register Size Modal -->
+
+            <!-- Register Product Modal -->
+            <div class="modal fade" id="registerProductModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5">Register Product</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+
+                            <div class="mb-2">
+                                <label for="" class="form-label">Product Name</label>
+                                <input type="text" class="form-control" id="regProducteName">
+                            </div>
+
+                            <div class="mb-2">
+                                <label for="" class="form-label">Product Description</label>
+                                <textarea type="text" class="form-control" id="regProducteDes" rows="5"></textarea>
+                            </div>
+
+                            <div class="mb-2">
+                                <label for="regProductCat" class="form-label">Category</label>
+                                <select id="regProductCat" class="form-select">
+                                    <option value="0">Select Category</option>
+                                    <?php
+                                    $rs = Database::search("SELECT * FROM `category`");
+                                    $num = $rs->num_rows;
+
+                                    for ($x = 0; $x < $num; $x++) {
+                                        $row = $rs->fetch_assoc();
+                                    ?>
+                                        <option value="<?php echo ($row["cat_id"]) ?>"><?php echo ($row["cat_name"]) ?></option>
+                                    <?php
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+
+                            <div class="mb-2">
+                                <label for="regProductCol" class="form-label">Color</label>
+                                <select id="regProductCol" class="form-select">
+                                    <option value="0">Select Color</option>
+                                    <?php
+                                    $rs = Database::search("SELECT * FROM `color`");
+                                    $num = $rs->num_rows;
+
+                                    for ($x = 0; $x < $num; $x++) {
+                                        $row = $rs->fetch_assoc();
+                                    ?>
+                                        <option value="<?php echo ($row["color_id"]) ?>"><?php echo ($row["color_name"]) ?></option>
+                                    <?php
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+
+                            <div class="mb-2">
+                                <label for="regProductBrand" class="form-label">Brand</label>
+                                <select id="regProductBrand" class="form-select">
+                                    <option value="0">Select Brand</option>
+                                    <?php
+                                    $rs = Database::search("SELECT * FROM `brand`");
+                                    $num = $rs->num_rows;
+
+                                    for ($x = 0; $x < $num; $x++) {
+                                        $row = $rs->fetch_assoc();
+                                    ?>
+                                        <option value="<?php echo ($row["brand_id"]) ?>"><?php echo ($row["brand_name"]) ?></option>
+                                    <?php
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+
+                            <div class="mb-2">
+                                <label for="regProductSize" class="form-label">Size</label>
+                                <select id="regProductSize" class="form-select">
+                                    <option value="0">Select Size</option>
+                                    <?php
+                                    $rs = Database::search("SELECT * FROM `size`");
+                                    $num = $rs->num_rows;
+
+                                    for ($x = 0; $x < $num; $x++) {
+                                        $row = $rs->fetch_assoc();
+                                    ?>
+                                        <option value="<?php echo ($row["size_id"]) ?>"><?php echo ($row["size_name"]) ?></option>
+                                    <?php
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+
+                            <div class="mb-2">
+                                <label for="" class="form-label">Product Image</label>
+                                <input type="file" id="regProductImg" class="form-control">
+                            </div>
+
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary" id="regProductBtn">Add Product</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Register Product Modal -->
 
             <script src="../../assets/js/script.js"></script>
             <script src="../../assets/js/bootstrap.js"></script>
