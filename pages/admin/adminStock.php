37bd00a1bbdd1732d85b7eb0083f37c18777c61a
RYZEN AMD <151421332+RYZENRADEON@users.noreply.github.com>
2025-12-22 22:06:37 +0530

    added Register Stock Modal and modified

--- pages/admin/adminStock.php
+++ pages/admin/adminStock.php
@@ -17,7 +17,7 @@ if (isset($_SESSION["admin"])) {
         <link rel="stylesheet" href="../../assets/css/style.css">
     </head>
 
-    <body id="adminProductPage" data-page="1">
+    <body id="adminStockPage" data-page="1">
         <!-- Include the admin header -->
         <?php include 'adminHeader.php'; ?>
         <!-- Include the admin header -->
@@ -29,14 +29,7 @@ if (isset($_SESSION["admin"])) {
                     <div class="row">
 
                         <div class="col-12 d-flex justify-content-center mb-2">
-                            <button class="btn btn-sm btn-primary mb-3" data-bs-toggle="modal" data-bs-target="#registerProductModal">Register Product</button>
-                        </div>
-
-                        <div class="col-6 offset-3 d-flex justify-content-center mb-3">
-                            <button class="btn btn-sm btn-primary me-2" data-bs-toggle="modal" data-bs-target="#registerBrandModal">Register New Brand</button>
-                            <button class="btn btn-sm btn-secondary me-2" data-bs-toggle="modal" data-bs-target="#registerColorModal">Register New Color</button>
-                            <button class="btn btn-sm btn-secondary me-2" data-bs-toggle="modal" data-bs-target="#registerCategoryModal">Register New Category</button>
-                            <button class="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target="#registerSizeModal">Register New Size</button>
+                            <button class="btn btn-sm btn-primary mb-3" data-bs-toggle="modal" data-bs-target="#registerStockModal">Register Stock</button>
                         </div>
 
                         <div class="mt-4 table-responsive" id="content">
@@ -52,6 +45,52 @@ if (isset($_SESSION["admin"])) {
             <!-- Include the admin footer -->
             <?php include 'adminFooter.php'; ?>
             <!-- Include the admin footer -->
+
+            <!-- Register Stock Modal -->
+            <div class="modal fade" id="registerStockModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5">Register Stock</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="mb-2">
+                                <label for="regStockPro" class="form-label">Select Product</label>
+                                <select id="regStockPro" class="form-select">
+                                    <option value="0">Select Product</option>
+                                    <?php
+                                    $rs = Database::search("SELECT * FROM `product_details` WHERE `status_id` = 1");
+                                    $num = $rs->num_rows;
+
+                                    for ($x = 0; $x < $num; $x++) {
+                                        $row = $rs->fetch_assoc();
+                                    ?>
+                                        <option value="<?php echo ($row["product_id"]) ?>"><?php echo ($row["product_name"] . ' ' . $row["color_name"] . ' ' . $row["cat_name"] . ' ' . $row["size_name"] . ' ' . $row["brand_name"]); ?></option>
+                                    <?php
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+
+                            <div class="mb-2">
+                                <label for="regStockPrice" class="form-label">Unit Price</label>
+                                <input type="number" class="form-control" name="" id="regStockPrice">
+                            </div>
+
+                            <div class="mb-2">
+                                <label for="regStockQty">Quantity</label>
+                                <input type="number" class="form-control" name="" id="regStockQty">
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary" id="regStockBtn">Add Stock</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Register Stock Modal -->
 
             <script src="../../assets/js/script.js"></script>
             <script src="../../assets/js/bootstrap.js"></script>
