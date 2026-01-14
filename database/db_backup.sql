720c2420137a7833ea4e69ed1844878fe49bb9e7
RYZEN AMD <r5rx5600ma@gmail.com>
2026-01-14 12:45:14 +0530

    db updated and modified

--- database/db_backup.sql
+++ database/db_backup.sql
@@ -14,23 +14,23 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Dumping data for table online_store.brand: ~3 rows (approximately)
-INSERT INTO `brand` (`brand_id`, `brand_name`) VALUES
+-- Dumping data for table online_store.brand: ~4 rows (approximately)
+REPLACE INTO `brand` (`brand_id`, `brand_name`) VALUES
 	(1, 'Nike'),
 	(2, 'Moose'),
 	(3, 'Adidas'),
 	(4, 'CP');
 
--- Dumping data for table online_store.category: ~4 rows (approximately)
-INSERT INTO `category` (`cat_id`, `cat_name`) VALUES
+-- Dumping data for table online_store.category: ~5 rows (approximately)
+REPLACE INTO `category` (`cat_id`, `cat_name`) VALUES
 	(1, 'Tshirts'),
 	(2, 'Jeens'),
 	(3, 'Denims'),
 	(4, 'Skirt'),
 	(5, 'Skinny');
 
--- Dumping data for table online_store.color: ~4 rows (approximately)
-INSERT INTO `color` (`color_id`, `color_name`) VALUES
+-- Dumping data for table online_store.color: ~5 rows (approximately)
+REPLACE INTO `color` (`color_id`, `color_name`) VALUES
 	(1, 'Black'),
 	(2, 'White'),
 	(3, 'Blue'),
@@ -38,20 +38,20 @@ INSERT INTO `color` (`color_id`, `color_name`) VALUES
 	(5, 'Pink');
 
 -- Dumping data for table online_store.product: ~10 rows (approximately)
-INSERT INTO `product` (`product_id`, `product_name`, `description`, `img`, `color_id`, `cat_id`, `size_id`, `brand_id`, `status_id`) VALUES
+REPLACE INTO `product` (`product_id`, `product_name`, `description`, `img`, `color_id`, `cat_id`, `size_id`, `brand_id`, `status_id`) VALUES
 	(1, 'Black shirt', 'Black shirt', '/Online-Store/assets/images/product/69446951ab0a4images.jpg', 1, 1, 3, 2, 1),
-	(2, 'Black shirt', 'Black shirt', 'assets/images/product/6944698b26f1bimages.jpg', 1, 1, 3, 2, 2),
+	(2, 'Black shirt', 'Black shirt', 'assets/images/product/6944698b26f1bimages.jpg', 1, 1, 3, 2, 1),
 	(3, 'Black shirt', 'Black shirt', 'assets/images/product/694469fe5b6fcimages.jpg', 1, 1, 3, 2, 1),
 	(4, 'Black shirt', 'Black shirt', 'assets/images/product/69446a38c0502images.jpg', 1, 1, 3, 2, 1),
 	(5, 'Black shirt', 'Black shirt', 'assets/images/product/69446ab0f0e8fimages.jpg', 1, 1, 3, 2, 1),
 	(6, 'Black shirt', 'Black Shirt', '../../assets/images/product/69446c0024d01images.jpg', 1, 1, 3, 2, 1),
-	(7, 'Black shirt', 'CK Black Shirt', '../../assets/images/product/69446d0b0bde9images (1).jpg', 1, 2, 3, 4, 2),
+	(7, 'Black shirt', 'CK Black Shirt', '../../assets/images/product/69446d0b0bde9images (1).jpg', 1, 2, 3, 4, 1),
 	(8, 'new shirt', 'new desc', '../../assets/images/product/694dd286400a0images.jpg', 2, 2, 3, 1, 1),
-	(9, 'new shirt', 'new shirt', '../../assets/images/product/6952d6296972467d6e4f0bfdec047143d3dd8-nike-mens-short-sleeve-logo-swoosh.jpg', 2, 1, 5, 1, 2),
+	(9, 'new shirt', 'new shirt', '../../assets/images/product/6952d6296972467d6e4f0bfdec047143d3dd8-nike-mens-short-sleeve-logo-swoosh.jpg', 2, 1, 5, 1, 1),
 	(10, 'Moose Denim XL RED', 'new denim', '../../assets/images/product/6952d732dccd4images (2).jpg', 4, 3, 5, 2, 1);
 
--- Dumping data for table online_store.size: ~6 rows (approximately)
-INSERT INTO `size` (`size_id`, `size_name`) VALUES
+-- Dumping data for table online_store.size: ~7 rows (approximately)
+REPLACE INTO `size` (`size_id`, `size_name`) VALUES
 	(1, 'XS'),
 	(2, 'S'),
 	(3, 'M'),
@@ -61,17 +61,26 @@ INSERT INTO `size` (`size_id`, `size_name`) VALUES
 	(7, 'XXXL');
 
 -- Dumping data for table online_store.status: ~2 rows (approximately)
-INSERT INTO `status` (`id`, `status`) VALUES
+REPLACE INTO `status` (`id`, `status`) VALUES
 	(1, 'active'),
 	(2, 'inactive');
 
--- Dumping data for table online_store.stock: ~2 rows (approximately)
-INSERT INTO `stock` (`stock_id`, `price`, `qty`, `status_id`, `product_id`) VALUES
-	(1, 250.5, '150', 1, 7),
-	(2, 275.25, '152', 1, 1);
+-- Dumping data for table online_store.stock: ~9 rows (approximately)
+REPLACE INTO `stock` (`stock_id`, `price`, `qty`, `status_id`, `product_id`) VALUES
+	(1, 250.5, '150', 2, 7),
+	(2, 275.25, '152', 2, 1),
+	(3, 256, '300', 1, 10),
+	(4, 500, '154', 1, 9),
+	(5, 890, '700', 1, 8),
+	(6, 587.25, '852', 1, 8),
+	(7, 597.57, '650', 1, 9),
+	(8, 789.35, '870', 1, 8),
+	(9, 650.36, '555', 1, 9),
+	(10, 587.69, '587', 1, 10),
+	(11, 741.65, '665', 1, 8);
 
 -- Dumping data for table online_store.users: ~13 rows (approximately)
-INSERT INTO `users` (`id`, `fname`, `lname`, `mobile`, `email`, `password`, `user_type_id`, `v_code`, `status_id`) VALUES
+REPLACE INTO `users` (`id`, `fname`, `lname`, `mobile`, `email`, `password`, `user_type_id`, `v_code`, `status_id`) VALUES
 	(1, 'fname', 'lname', '0777777771', 'email@gmail.com', '00001', 2, NULL, 1),
 	(2, 'fname0', 'lname0', '0777777777', 'r5rx5600ma@gmail.com', '00003', 1, NULL, 1),
 	(3, 'fname1', 'lname1', '0781234567', 'email1@gmail.com', '00001', 2, NULL, 1),
@@ -87,7 +96,7 @@ INSERT INTO `users` (`id`, `fname`, `lname`, `mobile`, `email`, `password`, `use
 	(13, 'fname11', 'lname11', '0786541238', 'email11@gmail.com', '000011', 2, NULL, 1);
 
 -- Dumping data for table online_store.user_type: ~2 rows (approximately)
-INSERT INTO `user_type` (`id`, `name`) VALUES
+REPLACE INTO `user_type` (`id`, `name`) VALUES
 	(1, 'Admin'),
 	(2, 'User');
 
