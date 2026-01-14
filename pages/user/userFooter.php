e11c06582aa3c15778d6e39ae7e4e4fafcba05fc
RYZEN AMD <r5rx5600ma@gmail.com>
2026-01-14 12:51:36 +0530

    added user footer and modified

---
+++ pages/user/userFooter.php
@@ -0,0 +1,13 @@
+<div class="container-fluid bg-body-secondary py-3">
+    <div class="row">
+        <div class="col-12">
+            <div class="d-flex align-items-center justify-content-around">
+                <img src="../../assets/images/logo/logo01.png" alt="">
+                <div>
+                    <span class="fs-2 fw-bold">Online Store</span>
+                    <p>&copy; 2024 Online Store. All rights reserved.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
