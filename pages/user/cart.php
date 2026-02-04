79bb49f71693e619f20be0a42c00841fbfd4b597
RYZEN AMD <r5rx5600ma@gmail.com>
2026-02-04 22:39:01 +0530

    added cart.php to load cart

---
+++ pages/user/cart.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+include '../../config/connection.php';
+if (!isset($_SESSION["user"])) {
+    header("Location: ../../index.php");
+    exit;
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cart | Online Store</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <link rel="icon" href="../../assets/images/logo/logo01.png">
+    <link rel="stylesheet" href="../../assets/css/style.css">
+    <link rel="stylesheet" href="../../assets/css/bootstrap.css">
+</head>
+
+<body id="cart">
+    <!-- header -->
+    <?php include 'userHeader.php'; ?>
+    <!-- header -->
+
+    <div class="container" id="content">
+
+        <!-- cart item -->
+
+        <!-- cart item -->
+
+    </div>
+
+    <!-- Include the user footer -->
+    <?php include 'userFooter.php'; ?>
+    <!-- Include the user footer -->
+
+    <script src="../../assets/js/bootstrap.js"></script>
+    <script src="../../assets/js/script.js"></script>
+</body>
+
+</html>
