fe240fe377d661f44b4a9332f2bc80f9e4b3ff76
RYZEN AMD <r5rx5600ma@gmail.com>
2026-02-25 17:08:19 +0530

    amodified UI

--- pages/admin/adminSignIn.php
+++ pages/admin/adminSignIn.php
@@ -1,56 +1,144 @@
-<!DOCTYPE html>
-<html lang="en" data-bs-theme="dark">
+<?php
+session_status();
+?>
+
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="" />
     <title>Admin Sign In | Online Store</title>
 
     <link rel="icon" href="../../assets/images/logo/logo01.png">
-    <link rel="stylesheet" href="../../assets/css/style.css">
-    <link rel="stylesheet" href="../../assets/css/bootstrap.css">
+    <link href="../../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="../../assets/css/admin.css" rel="stylesheet" />
+    <style>
+        .bi {
+            vertical-align: -0.125em;
+            fill: currentColor;
+        }
+
+        .bd-mode-toggle .bi {
+            width: 1em;
+            height: 1em;
+        }
+
+        .bd-mode-toggle .dropdown-menu .active .bi {
+            display: block !important;
+        }
+    </style>
 </head>
 
-<body>
+<body class="d-flex align-items-center py-4 bg-body-tertiary">
+
+    <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
+        <symbol id="check2" viewBox="0 0 16 16">
+            <path
+                d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z">
+            </path>
+        </symbol>
+        <symbol id="circle-half" viewBox="0 0 16 16">
+            <path d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z"></path>
+        </symbol>
+        <symbol id="moon-stars-fill" viewBox="0 0 16 16">
+            <path
+                d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278z">
+            </path>
+            <path
+                d="M10.794 3.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387a1.734 1.734 0 0 0-1.097 1.097l-.387 1.162a.217.217 0 0 1-.412 0l-.387-1.162A1.734 1.734 0 0 0 9.31 6.593l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387a1.734 1.734 0 0 0 1.097-1.097l.387-1.162zM13.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.156 1.156 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.156 1.156 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732L13.863.1z">
+            </path>
+        </symbol>
+        <symbol id="sun-fill" viewBox="0 0 16 16">
+            <path
+                d="M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z">
+            </path>
+        </symbol>
+    </svg>
+
+    <div class="dropdown position-fixed top-0 end-0 mt-3 me-3 bd-mode-toggle">
+        <button class="btn btn-primary py-2 dropdown-toggle d-flex align-items-center" id="bd-theme" type="button"
+            aria-expanded="false" data-bs-toggle="dropdown" aria-label="Toggle theme (auto)">
+            <svg class="bi my-1 theme-icon-active" aria-hidden="true">
+                <use href="#circle-half"></use>
+            </svg>
+            <span class="visually-hidden" id="bd-theme-text">Toggle theme</span>
+        </button>
+        <ul class="dropdown-menu dropdown-menu-end shadow" aria-labelledby="bd-theme-text">
+            <li>
+                <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="light"
+                    aria-pressed="false">
+                    <svg class="bi me-2 opacity-50" aria-hidden="true">
+                        <use href="#sun-fill"></use>
+                    </svg>
+                    Light
+                    <svg class="bi ms-auto d-none" aria-hidden="true">
+                        <use href="#check2"></use>
+                    </svg>
+                </button>
+            </li>
+            <li>
+                <button type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dark"
+                    aria-pressed="false">
+                    <svg class="bi me-2 opacity-50" aria-hidden="true">
+                        <use href="#moon-stars-fill"></use>
+                    </svg>
+                    Dark
+                    <svg class="bi ms-auto d-none" aria-hidden="true">
+                        <use href="#check2"></use>
+                    </svg>
+                </button>
+            </li>
+            <li>
+                <button type="button" class="dropdown-item d-flex align-items-center active" data-bs-theme-value="auto"
+                    aria-pressed="true">
+                    <svg class="bi me-2 opacity-50" aria-hidden="true">
+                        <use href="#circle-half"></use>
+                    </svg>
+                    Auto
+                    <svg class="bi ms-auto d-none" aria-hidden="true">
+                        <use href="#check2"></use>
+                    </svg>
+                </button>
+            </li>
+        </ul>
+    </div>
+
     <!-- ADMIN SIGN IN BOX -->
-    <div class="container">
-        <div class="row vh-100 d-flex justify-content-center align-items-center">
-            <div class="col-10 col-lg-4">
-                <div class="row card">
-                    <div class="card-body">
+    <main class="form-signin w-100 m-auto card rounded-4 shadow border-0 bg-opacity-50">
 
-                        <div class="col-12 text-center">
-                            <h2>Admin Sign In</h2>
-                        </div>
+        <div class="col-12">
+            <H2 class="text-cener">Admin Sign In</H2>
+        </div>
 
-                        <div class="col-12 mb-3">
-                            <label for="adSiEmail" class="form-label">Email Address</label>
-                            <input type="email" class="form-control" id="adSiEmail" placeholder="admin@onlinestore.com">
-                        </div>
+        <div class="col-12 mb-3">
+            <label for="adSiEmail" class="form-label">Email Address</label>
+            <input type="email" class="form-control" id="adSiEmail" placeholder="admin@onlinestore.com">
+        </div>
 
-                        <div class="col-12 mb-3">
-                            <label for="adSiPassword" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="adSiPassword" placeholder="*********">
-                        </div>
+        <div class="col-12 mb-3">
+            <label for="adSiPassword" class="form-label">Password</label>
+            <input type="password" class="form-control" id="adSiPassword" placeholder="*********">
+        </div>
 
-                        <div class="errorMsgDiv3 d-none">
-                            <div class="alert alert-danger" id="errorMsg3">
-                                <!-- Error Message -->
-                            </div>
-                        </div>
-
-                        <div class="d-grid mb-3">
-                            <button class="btn btn-secondary" id="adSigninBtn">Sign In</button>
-                        </div>
-
-                    </div>
-                </div>
+        <div class="errorMsgDiv3 d-none">
+            <div class="alert alert-danger" id="errorMsg3">
+                <!-- Error Message -->
             </div>
         </div>
-    </div>
+
+        <div class="d-grid mb-3">
+            <button class="btn btn-secondary" id="adSigninBtn">Sign In</button>
+        </div>
+
+    </main>
     <!-- ADMIN SIGN IN BOX -->
 
-    <script src="../../assets/js/script.js"></script>
+    <script src="../../assets/bootstrap/js/bootstrap.bundle.min.js" class="astro-vvvwv3sm"></script>
+    <script src="../../assets/js/index.js" class="astro-vvvwv3sm"></script>
+    <script type="module" src="../../assets/js/auth.js"></script>
+
 </body>
 
 </html>
